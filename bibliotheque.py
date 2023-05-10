b2218535ee4971e20b24b7d15f999443d350485b
NicolasDrp <drapier.nicolas07@gmail.com>
2023-05-10 09:34:04 +0200

    ajout : commentaire des fonctions

--- bibliotheque.py
+++ bibliotheque.py
@@ -9,29 +9,29 @@ livre = [
     }
 ]
 
-
+# Fonction qui permet à l'utilisateur d'afficher les livres disponnible
 def afficher_livres():
     for i in livre:
         print(i["Titre"]+" : "+i["Auteur"])
 
-
+# Fonction qui permet à l'utilisateur d'ajouter un livre à la bibliotheque
 def ajouter_livre(titre, auteur):
     livre.append({"Titre": titre,
                   "Auteur": auteur})
 
-
+# Fonction qui permet à l'utilisateur de supprimer un livre de la bibliotheque
 def supprimer_livre(titre):
     for i in livre:
         if i["Titre"].lower() == titre.lower():
             livre.remove(i)
 
-
+# Fonction qui permet à l'utilisateur de rechercher un livre dans la bibliotheque a partir de son titre
 def Rechercher_livre_titre(titre):
     for i in livre:
         if i["Titre"].lower() == titre.lower():
             print(i["Titre"]+" : "+i["Auteur"])
 
-
+# Fonction qui permet à l'utilisateur de trier les livres de la bibliotheque a partir de leurs titres
 def Trier_livre_titre():
     longueur = len(livre)
     for i in range(longueur):
@@ -41,7 +41,7 @@ def Trier_livre_titre():
 
     print(f"Tri des livres par titre: {livre}")
 
-
+# Fonction qui permet à l'utilisateur de trier les livres de la bibliotheque a partir de leurs auteurs
 def Trier_livre_auteur():
     longueur = len(livre)
     for i in range(longueur):
@@ -51,15 +51,21 @@ def Trier_livre_auteur():
 
     print(f"Tri des livres par auteur: {livre}")
 
-
+# Fonction qui permet à l'utilisateur de choisir si il veut continuer ou arreter le programme
 def choix_menu():
     choix = 0
-    while choix < 1 or choix > 5:
-        choix = int(input("Retourner au menu?\n1) Oui\n2) Non"))
+    while choix < 1 or choix > 2:
+        print("___________________________________________________________________")
+        print("|----------------------Choisissez une option----------------------|")
+        print("|-------------------1) Retrourner au menu principal---------------|")
+        print("|-------------------2) Arrêter le programme-----------------------|")
+        print("|_________________________________________________________________|")
+        choix = int(input())
         if choix == 1:
             menu()
 
 
+# Fonction principal qui permet à l'utilisateur de choisir quel action il veut effectuer
 def menu():
 
     choix = 0
@@ -73,7 +79,7 @@ def menu():
         print("|-------------------5) Trier les livres---------------------------|")
         print("|_________________________________________________________________|")
 
-        choix = int(input("fais choix"))
+        choix = int(input())
 
     match choix:
         case 1:
@@ -93,10 +99,13 @@ def menu():
             Rechercher_livre_titre(titre)
             choix_menu()
         case 5:
-            choix = int(input("Voulez-vous trier par :\n1) Titre\n2) Auteur"))
-
-            Trier_livre_titre()
-            Trier_livre_auteur()
+            choix = 0
+            while choix < 1 or choix > 2:
+                choix = int(input("Voulez-vous trier par :\n1) Titre\n2) Auteur"))
+                if choix == 1:
+                    Trier_livre_titre()
+                else:
+                    Trier_livre_auteur()
             choix_menu()
 
 
