303777353272e1ee7f6051bc0226714c5c6e181e
NicolasDrp <drapier.nicolas07@gmail.com>
2023-05-10 10:48:20 +0200

    fini

--- bibliotheque.py
+++ bibliotheque.py
@@ -10,78 +10,85 @@ livre = [
 ]
 
 # Fonction qui permet à l'utilisateur d'afficher les livres disponnible
+
+
 def afficher_livres():
     for i in livre:
         print(i["Titre"]+" : "+i["Auteur"])
 
 # Fonction qui permet à l'utilisateur d'ajouter un livre à la bibliotheque
+
+
 def ajouter_livre(titre, auteur):
     livre.append({"Titre": titre,
                   "Auteur": auteur})
 
 # Fonction qui permet à l'utilisateur de supprimer un livre de la bibliotheque
+
+
 def supprimer_livre(titre):
     for i in livre:
         if i["Titre"].lower() == titre.lower():
             livre.remove(i)
 
 # Fonction qui permet à l'utilisateur de rechercher un livre dans la bibliotheque a partir de son titre
+
+
 def Rechercher_livre_titre(titre):
     for i in livre:
         if titre.lower() in i["Titre"].lower():
             print(i["Titre"]+" : "+i["Auteur"])
 
-# Fonction qui permet à l'utilisateur de trier les livres de la bibliotheque a partir de leurs titres
-def Trier_livre_titre():
+# Fonction qui permet à l'utilisateur de trier les livres de la bibliotheque a partir de leurs titres ou leurs auteurs
+
+
+def Trier_livre_type(type):
     longueur = len(livre)
     for i in range(longueur):
         for j in range(longueur-i-1):
-            if livre[j]["Titre"] > livre[j+1]["Titre"]:
+            if livre[j][type] > livre[j+1][type]:
                 livre[j], livre[j+1] = livre[j+1], livre[j]
 
-    print("Tri des livres par titre :")
-    afficher_livres()
-
-# Fonction qui permet à l'utilisateur de trier les livres de la bibliotheque a partir de leurs auteurs
-def Trier_livre_auteur():
-    longueur = len(livre)
-    for i in range(longueur):
-        for j in range(longueur-i-1):
-            if livre[j]["Auteur"] > livre[j+1]["Auteur"]:
-                livre[j], livre[j+1] = livre[j+1], livre[j]
-
-    print("Tri des livres par auteur :")
+    print(f"Tri des livres par {type} :")
     afficher_livres()
 
 # Fonction qui permet à l'utilisateur de choisir si il veut continuer ou arreter le programme
+
+
 def choix_menu():
     choix = 0
-    while choix < 1 or choix > 2:
-        print("___________________________________________________________________")
-        print("|----------------------Choisissez une option----------------------|")
-        print("|-------------------1) Retrourner au menu principal---------------|")
-        print("|-------------------2) Arrêter le programme-----------------------|")
-        print("|_________________________________________________________________|")
-        choix = int(input())
-        if choix == 1:
-            menu()
+    while choix == 0 or choix < 1 or choix > 2:
+        try:
+            print("___________________________________________________________________")
+            print("|----------------------Choisissez une option----------------------|")
+            print("|-------------------1) Retrourner au menu principal---------------|")
+            print("|-------------------2) Arrêter le programme-----------------------|")
+            print("|_________________________________________________________________|")
+            choix = int(input())
+            if choix == 1:
+                menu()
+        except:
+            print("Veuillez rentrer un chiffre valide")
 
 
 # Fonction principal qui permet à l'utilisateur de choisir quel action il veut effectuer
 def menu():
 
     choix = 0
-    while choix < 1 or choix > 5:
-        print("___________________________________________________________________")
-        print("|----------------------Choisissez une option----------------------|")
-        print("|-------------------1) Afficher tout les livres-------------------|")
-        print("|-------------------2) Ajouter un livre---------------------------|")
-        print("|-------------------3) Supprimer un livre-------------------------|")
-        print("|-------------------4) Rechercher un livre------------------------|")
-        print("|-------------------5) Trier les livres---------------------------|")
-        print("|_________________________________________________________________|")
+    while choix ==0 or choix < 1 or choix > 5:
+        try:
+            print("___________________________________________________________________")
+            print("|----------------------Choisissez une option----------------------|")
+            print("|-------------------1) Afficher tout les livres-------------------|")
+            print("|-------------------2) Ajouter un livre---------------------------|")
+            print("|-------------------3) Supprimer un livre-------------------------|")
+            print("|-------------------4) Rechercher un livre------------------------|")
+            print("|-------------------5) Trier les livres---------------------------|")
+            print("|_________________________________________________________________|")
+            choix = int(input())
+        except:
+            print("Veuillez rentrer un chiffre valide")
 
-        choix = int(input())
 
     match choix:
         case 1:
@@ -103,11 +110,12 @@ def menu():
         case 5:
             choix = 0
             while choix < 1 or choix > 2:
-                choix = int(input("Voulez-vous trier par :\n1) Titre\n2) Auteur"))
+                choix = int(
+                    input("Voulez-vous trier par :\n1) Titre\n2) Auteur"))
                 if choix == 1:
-                    Trier_livre_titre()
+                    Trier_livre_type("Titre")
                 else:
-                    Trier_livre_auteur()
+                    Trier_livre_type("Auteur")
             choix_menu()
 
 
